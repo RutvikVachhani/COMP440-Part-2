809def1268dca52da4bdaa2e44ddf3e6d8378111
Devanshi  Patel <pdevanshi670@gmail.com>
2021-11-20 14:36:54 -0800

    commit

--- postCommentProcess.php
+++ postCommentProcess.php
@@ -23,6 +23,30 @@ $blogid = $_SESSION['blogid'];
     $cdate = date("Y-m-d");
     $postedby = $_SESSION['User'];
 
+    //Cannot post of count > 3
+    $cCount = "SELECT COUNT(*) from comments WHERE cdate = '$cdate' AND posted_by = '$postedby';" ;
+    $count = mysqli_query($conn, $cCount);
+        
+    while($row = mysqli_fetch_array($count)) {
+        if($row['COUNT(*)'] >= 3){
+         $_SESSION['messages'][] = 'You cannot comment as you have exceeded the daily limit';
+         header('Location:postComment.php');
+         exit();
+        }
+    }
+
+    //Cannot post more than one comment for each blog 
+    $bComment = "SELECT COUNT(*) from comments INNER JOIN blogs ON comments.blogid = blogs.blogid; ";
+    $bComCount = mysqli_query($conn, $bComment);
+
+    while($row = mysqli_fetch_array($bComCount)) {
+        if($row['COUNT(*)'] >= 1){
+         $_SESSION['messages'][] = 'You cannot comment as you have already commented on this blog';
+         header('Location:postComment.php');
+         exit();
+        }
+    }
+
     $sql = "INSERT INTO `comments`(`sentiment`, `description`, `cdate`, `blogid`, `posted_by`) VALUES ('$selected', '$comment', '$cdate', '$blogid', '$postedby');" ;
     $result = mysqli_query($conn, $sql);
 
